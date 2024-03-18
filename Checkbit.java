70c77f6e7f558eba43e17f017c847159ac640cba
Tridip Kundu <77205163+TRIDIP220@users.noreply.github.com>
2024-03-18 13:33:37 +0530

    Create Checkbit.java

---
+++ Checkbit.java
@@ -0,0 +1,14 @@
+# BIT MAGIC
+
+
+class CheckBit
+{
+    // Function to check if Kth bit is set or not.
+    static boolean checkKthBit(int n, int k)
+    {
+        
+        return ((n >> (k)) & 1) == 1;
+        
+    }
+    
+}
